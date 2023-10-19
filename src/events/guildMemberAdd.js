867015201464df375f8bcd4490d345bd3f233216
RaidoxxMPH <suporterdx@gmail.com>
2023-10-19 15:31:38 -0300

    Removed Jimp Image

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -26,6 +26,7 @@ const fontPath = path.join(
   "DroidSansBold.fnt"
 );
 const savePath = path.join(__dirname, "..", "assets", "images", "avatar.png");
+const channelID = "1148780968841322508";
 
 client.on("guildMemberAdd", async (member) => {
   console.log(member.username);
@@ -39,36 +40,62 @@ client.on("guildMemberAdd", async (member) => {
   const fundo = await Jimp.read(backgroundPath);
   const composite = await Jimp.read(backgroundCompositePath);
 
-  avatar
-    .then((avatar) => {
-      avatar.resize(580, 580);
-      mask.resize(580, 580);
-      avatar.mask(mask);
-      fundo.composite(avatar, 910, 95);
-      fundo.print(fonte, 1100, 850, member.nickname || member.user.username);
-      fundo.composite(composite, 0, 0).write(savePath);
+  const welcome_embed = new EmbedBuilder()
+    .setDescription(`Bem vindo ao servidor ${member.user}!`)
+    .setColor("#23272A");
+  const channel = member.guild.channels.cache.get(channelID);
 
-      const welcome_embed = new EmbedBuilder()
-        .setDescription(`Bem vindo ao servidor ${member.user}!`)
-        .setColor("#23272A")
-        .setImage("attachment://avatar.png");
-
-      const channel = member.guild.channels.cache.get("1148712892124897441"); // Substitua pelo ID do canal correto
-
-      if (channel) {
-        channel.send({
-          embeds: [welcome_embed],
-          files: [{ attachment: savePath, name: "avatar.png" }],
-        });
-      } else {
-        console.log("Canal não encontrado.");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
+  if (channel) {
+    channel.send({
+      embeds: [welcome_embed],
     });
+  } else {
+    console.log("Canal não encontrado.");
+  }
+
+  // avatar
+  //   .then((avatar) => {
+  //     avatar.resize(580, 580);
+  //     mask.resize(580, 580);
+  //     avatar.mask(mask);
+  //     fundo.composite(avatar, 910, 95);
+  //     fundo.print(fonte, 1100, 850, member.nickname || member.user.username);
+  //     fundo.composite(composite, 0, 0).write(savePath);
+
+  //     const welcome_embed = new EmbedBuilder()
+  //       .setDescription(`Bem vindo ao servidor ${member.user}!`)
+  //       .setColor("#23272A")
+  //       .setImage("attachment://avatar.png");
+
+  //     const channel = member.guild.channels.cache.get("1148712892124897441");
+
+  //     if (channel) {
+  //       channel.send({
+  //         embeds: [welcome_embed],
+  //         files: [{ attachment: savePath, name: "avatar.png" }],
+  //       });
+  //     } else {
+  //       console.log("Canal não encontrado.");
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
 client.on("guildMemberRemove", async (interaction) => {
-  console.log(interaction.user.username);
+  const bye_embed = new EmbedBuilder()
+    .setDescription(`Até mais ${interaction.user}!`)
+    .setColor("#23272A");
+
+  const channel = interaction.guild.channels.cache.get(channelID);
+
+  if (channel) {
+    channel.send({
+      embeds: [bye_embed],
+    });
+  } else {
+    console.log("Canal não encontrado.");
+  }
+
 });
