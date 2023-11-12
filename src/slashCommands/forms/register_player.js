c6a91f2736cf2a8004e0269e8401e1eabb6be51e
RaidoxxMPH <suporterdx@gmail.com>
2023-11-12 15:31:41 -0300

    Update register_player.js

--- src/slashCommands/forms/register_player.js
+++ src/slashCommands/forms/register_player.js
@@ -17,7 +17,7 @@ module.exports = {
   cooldown: 3000,
   run: async (client, interaction) => {
 
-    if (!interaction.member.roles.cache.get(suportRole)) {
+    if (!interaction.member.roles.cache.get(suportRole) || !interaction.member.permissions.has(PermissionsBitField.ADMINISTRATOR)) {
       return interaction.reply({
         content: `${interaction.user}, Você não tem permissão para usar isso!`,
         ephemeral: true,
