9114bfab97a55adb0880d5febcef157d75babd31
RaidoxxMPH <suporterdx@gmail.com>
2023-12-17 15:55:16 -0300

    Update Database.js

--- src/data/Database.js
+++ src/data/Database.js
@@ -6,19 +6,20 @@ class Database {
     }
   
     connect() {
-        if (this.connection) return;
+        console.log("Sem data")
+        // if (this.connection) return;
 
-        this.connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database : process.env.DB_NAME,
-        });
+        // this.connection = mysql.createConnection({
+        //     host: process.env.DB_HOST,
+        //     user: process.env.DB_USER,
+        //     password: process.env.DB_PASS,
+        //     database : process.env.DB_NAME,
+        // });
 
-        this.connection.connect((err) => {
-            if (err) throw err;
-            console.log("Connected to the database!");
-        });
+        // this.connection.connect((err) => {
+        //     if (err) throw err;
+        //     console.log("Connected to the database!");
+        // });
     }
 
     async query(query, params) {
